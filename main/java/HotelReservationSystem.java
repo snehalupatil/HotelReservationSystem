2251116fe3e38006a2f91f4a8d46283c9874dbd9
snehalupatil <patilsnehal4575@gmail.com>
2021-05-21 10:01:27 +0530

    Added: Hotel Name and Rate for regular customer is added

--- main/java/HotelReservationSystem.java
+++ main/java/HotelReservationSystem.java
@@ -1,5 +1,20 @@
-public class HotelReservationSystem {
-    public static void main(String[] args) {
-        System.out.println("Welcome to HotelReservation SYstem");
+import java.util.ArrayList;
+
+public class HotelReservationSystem  {
+    ArrayList<HotelInfo> hotelInfo=new ArrayList<HotelInfo>();
+
+    public  void addHotelNameAndRatesForRegularCustomer()  {
+
+        hotelInfo.add(new HotelInfo("LakeWood",110));
+        hotelInfo.add(new HotelInfo("BridgeWood",160));
+        hotelInfo.add(new HotelInfo("RidgeWood",220));
+        hotelInfo.stream().forEach(System.out::println);
+    }
+}
+
+    public static void main(String[] args){
+
+        System.out.println("Welcome to Hotel Reservation System");
+        addHotelNameAndRatesForRegularCustomer();
     }
 }
